6a71ecb4fbd2c160beed0b0d615a62effa1ecf6c
Shara Dinakar <sharadinakar@gmail.com>
2026-07-16 17:22:09 +0530

    modified navbar desktop version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ function Navbar() {
         </div>
 
         {/* Desktop Menu */}
-        <div className="hidden md:flex items-center gap-8 ml-auto">
+        <div className="hidden md:flex items-center gap-8 ml-auto" align ="center">
 
           <ul className="flex gap-8 text-gray-700 font-medium">
             <li className="hover:bg-green-700 hover:text-white cursor-pointer px-4 py-2 rounded-full transition-all duration-300">
