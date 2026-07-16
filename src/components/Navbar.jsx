bafeb7c793f829dcdc413a433d9672ce17914a2e
Shara Dinakar <sharadinakar@gmail.com>
2026-07-16 17:27:53 +0530

    created an account button on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,6 +45,12 @@ function Navbar() {
             </li>
           </ul>
 
+          <button>
+            <span className="bg-green-700 text-white px-4 py-2 rounded-full hover:bg-green-800 transition-all duration-300">
+              create account
+            </span>
+          </button>
+
           
 
         </div>
